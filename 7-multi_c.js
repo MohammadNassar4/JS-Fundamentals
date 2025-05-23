b8400571f475346ab0e2c1ad7f015a0bc06872a7
mohammad nassar <mohammadnassar044@gmail.com>
2025-05-23 04:54:52 +0300

    Modified file number 7

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,8 +1,6 @@
 const { argv } = require('node:process');
 let arg = Number(argv[2]);
-if (argv[2] === undefined) {
-    console.log('Missing number of occurrences');
-} else if (isNaN(arg)) {
+if (argv[2] === undefined || isNaN(arg)) {
     console.log('Missing number of occurrences');
 } else {
     for (let i = 0; i < argv[2]; i++) {
